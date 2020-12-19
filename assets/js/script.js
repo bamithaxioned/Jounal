3f37ea24b2c7485ee80f7df74d715de185c97cb1
bamithprdxn <amithkumarb@prdxn.com>
2020-12-19 15:05:54 +0530

    js code  for header added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
-/* Author: 
-
-*/
+// header scroll
 
 
+window.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+    header.classList.toggle("active", window.scrollY > 0)
+})
+
+$('.category-block').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
